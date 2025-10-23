e8f522c9af27ac97f90505ceb650b07073432592
AnjezaSfishta <anjezasfishta@gmail.com>
2025-10-23 18:00:25 +0200

    Trajtimi i vlerave te zbrazeta dhe duplikateve

--- para_procesimi_i_te_dhenave.py
+++ para_procesimi_i_te_dhenave.py
@@ -24,3 +24,29 @@ for col in cat_cols:
 
 print("\nPas konvertimit:")
 print(df.dtypes)
+
+# 5. Kontrolli i vlerave të zbrazëta
+print("\nVlerat e zbrazëta:")
+print(df.isnull().sum())
+
+# 6. Trajtimi i vlerave të zbrazëta
+# Postal Code - mbushe sipas qytetit
+df['Postal Code'] = df.groupby('City', observed=True)['Postal Code'].transform(lambda x: x.fillna(x.mode()[0] if not x.mode().empty else np.nan))
+
+# Nëse akoma ka NaN, mbush me median
+df['Postal Code'] = df['Postal Code'].fillna(df['Postal Code'].median())
+
+# Datat – mbush me median
+df['Order Date'] = df['Order Date'].fillna(df['Order Date'].median())
+df['Ship Date'] = df['Ship Date'].fillna(df['Ship Date'].median())
+
+print("\nPas trajtimit të vlerave të zbrazëta:")
+print(df.isnull().sum())
+
+# 7. Duplikatet
+print("\nNumri i rreshtave të duplikuar:", df.duplicated().sum())
+df = df.drop_duplicates()
+
+df_cleaned_no_outliers = df.copy()
+df_cleaned_no_outliers.to_csv("cleaned_data_without_outliers.csv", index=False)
+print("✅ Dataset i pastruar (pa outliers) ruhet si 'cleaned_data_without_outliers.csv'")
